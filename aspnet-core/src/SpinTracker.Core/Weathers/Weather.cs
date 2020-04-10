6d080fad3e21696d7d61283ee427e92c084a65a1
Chris Tennant <chris>
2020-04-10 12:09:54 +0100

    adding first entity

---
+++ aspnet-core/src/SpinTracker.Core/Weathers/Weather.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpinTracker.Weathers
+{
+    public class Weather: Entity<int>
+    {
+        public int WeatherId { get; set; }
+        public string Pollen { get; set; }
+        public string Barometric { get; set; }
+        public int Temperature { get; set; }
+        public int WeatherTypeId { get; set; }
+    }
+}
--- aspnet-core/src/SpinTracker.EntityFrameworkCore/EntityFrameworkCore/SpinTrackerDbContext.cs
+++ aspnet-core/src/SpinTracker.EntityFrameworkCore/EntityFrameworkCore/SpinTrackerDbContext.cs
@@ -3,13 +3,15 @@ using Abp.Zero.EntityFrameworkCore;
 using SpinTracker.Authorization.Roles;
 using SpinTracker.Authorization.Users;
 using SpinTracker.MultiTenancy;
+using SpinTracker.Weathers;
 
 namespace SpinTracker.EntityFrameworkCore
 {
     public class SpinTrackerDbContext : AbpZeroDbContext<Tenant, Role, User, SpinTrackerDbContext>
     {
         /* Define a DbSet for each entity of the application */
-        
+        public DbSet<Weather> Weathers { get; set; }
+
         public SpinTrackerDbContext(DbContextOptions<SpinTrackerDbContext> options)
             : base(options)
         {
