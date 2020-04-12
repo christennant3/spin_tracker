879e0ff32caea9a6c8305edf752f299a897ff116
Chris Tennant <chris>
2020-04-12 16:03:30 +0100

    added home vue file

--- vue/src/views/home/home.vue
+++ vue/src/views/home/home.vue
@@ -1,12 +1,27 @@
 <template>
-    <div>
-        <Card dis-hover></Card>
-    </div>
+  <div>
+    <h1>Home Page</h1>
+    <Card dis-hover>Card</Card>
+  </div>
 </template>
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({
-  
-})
+  data: {
+      weathers: []
+  },
+  filters: {},
+  mounted() {
+
+
+      axios.get("/api/results").then(response => {
+      this.weathers = response.data;
+    })
+  },
+
+  methods: {},
+
+  computed: {}
+});
 </script>
 
