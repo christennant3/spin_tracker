27a1d102d95f505d43b33eb16c4ab2851154bf43
Chris Tennant <chris>
2020-04-16 21:56:51 +0100

    got a get request working

--- vue/src/views/home/home.vue
+++ vue/src/views/home/home.vue
@@ -1,22 +1,27 @@
 <template>
   <div>
     <h1>Home Page</h1>
-    <Card dis-hover>Card</Card>
+    <div v-for="weather in weathers" v-bind:key="weather.id">
+      <Card dis-hover>Pollen: {{ weather.pollen }}</Card>
+    </div>
   </div>
 </template>
 <script lang="ts">
 import Vue from "vue";
+import axios from "axios";
 export default Vue.extend({
-  data: {
+  data: function() {
+    return {
       weathers: []
+    };
   },
   filters: {},
   mounted() {
-
-
-      axios.get("/api/results").then(response => {
-      this.weathers = response.data;
-    })
+    axios
+      .get("http://localhost:21021/api/services/app/Weather/GetAll")
+      .then(response => {
+        this.weathers = response.data.result.items;
+      });
   },
 
   methods: {},
