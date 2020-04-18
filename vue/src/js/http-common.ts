93c9df04a678133769401a4247abd243efe403eb
Chris Tennant <chris>
2020-04-18 16:57:23 +0100

    tidy up

--- vue/src/js/http-common.ts
+++ vue/src/js/http-common.ts
@@ -1,11 +1,18 @@
 import axios from "axios";
 let url: string;
+
 if (process.env.NODE_ENV === "development") {
-  url = "http://localhost:21021/api/services/app/";
+
+    url = "http://localhost:21021/api/services/app/";
+    
 } else if (process.env.NODE_ENV === "production") {
-  url = "http://localhost:21021/api/services/app/";
+
+    url = "http://localhost:21021/api/services/app/";
+    
 } else {
-  url = "http://localhost:21021/api/services/app/";
+
+    url = "http://localhost:21021/api/services/app/";
+    
 }
 
 export const HTTP = axios.create({
