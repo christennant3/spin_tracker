0197b423e498bcba80820faecddc7248e116cab8
Chris Tennant <chris>
2020-04-18 16:59:01 +0100

    tidy

--- vue/src/js/http-common.ts
+++ vue/src/js/http-common.ts
@@ -1,20 +1,21 @@
 import axios from "axios";
+
 let url: string;
 
 if (process.env.NODE_ENV === "development") {
 
     url = "http://localhost:21021/api/services/app/";
-    
+
 } else if (process.env.NODE_ENV === "production") {
 
     url = "http://localhost:21021/api/services/app/";
-    
+
 } else {
 
     url = "http://localhost:21021/api/services/app/";
-    
+
 }
 
 export const HTTP = axios.create({
-  baseURL: url,
+    baseURL: url,
 });
